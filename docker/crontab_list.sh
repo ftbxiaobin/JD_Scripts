570221ecbb8b4ed471a2b3c07d63d65ffbc44ba1
849165048 <a849165048@gmail.com>
2022-05-24 20:50:25 +0800

    update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -6,7 +6,10 @@
 ##############短期活动##############
 #热爱奇旅
 33 0,6-23/2 * * * node /scripts/jd_19E.js >> /scripts/logs/jd_19E.log 2>&1
-
+#热爱奇旅互助版
+33 0,6-23/3 * * * node /scripts/jd_19E_help.js >> /scripts/logs/jd_19E_help.log 2>&1
+#热爱奇旅互助版
+11 20 * * * node /scripts/jd_19EPZ_help.js >> /scripts/logs/jd_19EPZ_help.log 2>&1
 #618个护抽奖机
 10 7 * * * node /scripts/jd_618cj.js >> /scripts/logs/jd_618cj.log 2>&1
 
